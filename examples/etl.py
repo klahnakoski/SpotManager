af5fad48d2e2d943fb344e818dec179c95896bf9
Kyle Lahnakoski <klahnakoski@mozilla.com>
2019-09-04 16:40:59 -0400

    remove self parameter

--- examples/etl.py
+++ examples/etl.py
@@ -43,7 +43,8 @@ class ETL(InstanceManager):
 
         if current_utility < pending / 20:
             # INCREASE
-            return max(minimum, Math.ceiling(pending / 20))   # ENSURE THERE IS PLENTY OF WORK BEFORE MACHINE IS DEPLOYED
+            # ENSURE THERE IS PLENTY OF WORK BEFORE MACHINE IS DEPLOYED
+            return max(minimum, Math.ceiling(pending / 20))
         else:
             # DECREASE
             target = max(minimum, min(current_utility, pending*2))
@@ -53,7 +54,7 @@ class ETL(InstanceManager):
         self,
         instance,   # THE boto INSTANCE OBJECT FOR THE MACHINE TO SETUP
         utility,    # THE utility OBJECT FOUND IN CONFIG
-        please_stop
+        please_stop=False
     ):
         if not self.settings.setup_timeout:
             Log.error("expecting instance.setup_timeout to prevent setup from locking")
@@ -62,86 +63,90 @@ class ETL(InstanceManager):
         with Connection(host=instance.ip_address, kwargs=self.settings.connect) as c:
             cpu_count = int(round(utility.cpu))
 
-            self._update_ubuntu_packages(c)
-            self._setup_etl_code(c)
-            self._add_private_file(c)
-            self._setup_etl_supervisor(c, cpu_count)
+            _update_ubuntu_packages(c)
+            _setup_etl_code(c)
+            _add_private_file(c)
+            _setup_etl_supervisor(c, cpu_count)
 
     def teardown(self, instance, please_stop):
         with Connection(host=instance.ip_address, kwargs=self.settings.connect) as conn:
             Log.note("teardown {{instance}}", instance=instance.id)
             conn.sudo("supervisorctl stop all", warn=True)
 
-    def _update_ubuntu_packages(self, conn):
-        conn.sudo("apt-get clean")
-        conn.sudo("dpkg --configure -a")
-        conn.sudo("apt-get clean")
-        conn.sudo("apt-get update")
 
-    def _setup_etl_code(self, conn):
-        conn.sudo("apt-get install -y python2.7")
+def _update_ubuntu_packages(conn):
+    conn.sudo("apt-get clean")
+    conn.sudo("dpkg --configure -a")
+    conn.sudo("apt-get clean")
+    conn.sudo("apt-get update")
 
-        if not conn.exists("/usr/local/bin/pip"):
-            conn.run("mkdir -p /home/ubuntu/temp")
 
-            with conn.cd("/home/ubuntu/temp"):
-                # INSTALL FROM CLEAN DIRECTORY
-                conn.run("wget https://bootstrap.pypa.io/get-pip.py")
-                conn.sudo("rm -fr ~/.cache/pip")  # JUST IN CASE THE DIRECTORY WAS MADE
-                conn.sudo("python2.7 get-pip.py")
+def _setup_etl_code(conn):
+    conn.sudo("apt-get install -y python2.7")
 
-        if not conn.exists("/home/ubuntu/ActiveData-ETL/README.md"):
-            with conn.cd("/home/ubuntu"):
-                conn.sudo("apt-get -yf install git-core")
-                conn.run('rm -fr /home/ubuntu/ActiveData-ETL')
-                conn.run("git clone https://github.com/klahnakoski/ActiveData-ETL.git")
-                conn.run("mkdir -p /home/ubuntu/ActiveData-ETL/results/logs")
+    if not conn.exists("/usr/local/bin/pip"):
+        conn.run("mkdir -p /home/ubuntu/temp")
 
-        with conn.cd("/home/ubuntu/ActiveData-ETL"):
-            conn.run("git checkout etl")
-
-            # pip install -r requirements.txt HAS TROUBLE IMPORTING SOME LIBS
+        with conn.cd("/home/ubuntu/temp"):
+            # INSTALL FROM CLEAN DIRECTORY
+            conn.run("wget https://bootstrap.pypa.io/get-pip.py")
             conn.sudo("rm -fr ~/.cache/pip")  # JUST IN CASE THE DIRECTORY WAS MADE
-            conn.sudo("pip install future")
-            conn.sudo("pip install BeautifulSoup")
-            conn.sudo("pip install MozillaPulse")
-            conn.sudo("pip install boto")
-            conn.sudo("pip install requests")
-            conn.sudo("pip install taskcluster")
-            conn.sudo("apt-get install -y python-dev")  # REQUIRED FOR psutil
-            conn.sudo("apt-get install -y build-essential")  # REQUIRED FOR psutil
-            conn.sudo("pip install psutil")
-            conn.sudo("pip install pympler")
-            conn.sudo("pip install -r requirements.txt")
+            conn.sudo("python2.7 get-pip.py")
 
-        Log.note("8")
-        conn.sudo("apt-get -y install python-psycopg2")
-
-    def _setup_etl_supervisor(self, conn, cpu_count):
-        # INSTALL supervsor
-        conn.sudo("apt-get install -y supervisor")
-        # with fabric_settings(warn=True:
-        conn.sudo("service supervisor start")
-
-        # READ LOCAL CONFIG FILE, ALTER IT FOR THIS MACHINE RESOURCES, AND PUSH TO REMOTE
-        conf_file = File("./examples/config/etl_supervisor.conf")
-        content = conf_file.read_bytes()
-        find = between(content, "numprocs=", "\n")
-        content = content.replace("numprocs=" + find + "\n", "numprocs=" + str(cpu_count) + "\n")
-        File("./temp/etl_supervisor.conf.alt").write_bytes(content)
-        conn.sudo("rm -f /etc/supervisor/conf.d/etl_supervisor.conf")
-        conn.put("./temp/etl_supervisor.conf.alt", '/etc/supervisor/conf.d/etl_supervisor.conf', use_sudo=True)
-        conn.run("mkdir -p /home/ubuntu/ActiveData-ETL/results/logs")
-
-        # POKE supervisor TO NOTICE THE CHANGE
-        conn.sudo("supervisorctl reread")
-        conn.sudo("supervisorctl update")
-
-    def _add_private_file(self, conn):
-        conn.run('rm -f /home/ubuntu/private.json')
-        conn.put('~/private_active_data_etl.json', '/home/ubuntu/private.json')
+    if not conn.exists("/home/ubuntu/ActiveData-ETL/README.md"):
         with conn.cd("/home/ubuntu"):
-            conn.run("chmod o-r private.json")
+            conn.sudo("apt-get -yf install git-core")
+            conn.run('rm -fr /home/ubuntu/ActiveData-ETL')
+            conn.run("git clone https://github.com/klahnakoski/ActiveData-ETL.git")
+            conn.run("mkdir -p /home/ubuntu/ActiveData-ETL/results/logs")
+
+    with conn.cd("/home/ubuntu/ActiveData-ETL"):
+        conn.run("git checkout etl")
+
+        # pip install -r requirements.txt HAS TROUBLE IMPORTING SOME LIBS
+        conn.sudo("rm -fr ~/.cache/pip")  # JUST IN CASE THE DIRECTORY WAS MADE
+        conn.sudo("pip install future")
+        conn.sudo("pip install BeautifulSoup")
+        conn.sudo("pip install MozillaPulse")
+        conn.sudo("pip install boto")
+        conn.sudo("pip install requests")
+        conn.sudo("pip install taskcluster")
+        conn.sudo("apt-get install -y python-dev")  # REQUIRED FOR psutil
+        conn.sudo("apt-get install -y build-essential")  # REQUIRED FOR psutil
+        conn.sudo("pip install psutil")
+        conn.sudo("pip install pympler")
+        conn.sudo("pip install -r requirements.txt")
+
+    Log.note("8")
+    conn.sudo("apt-get -y install python-psycopg2")
+
+
+def _setup_etl_supervisor(conn, cpu_count):
+    # INSTALL supervsor
+    conn.sudo("apt-get install -y supervisor")
+    # with fabric_settings(warn=True:
+    conn.sudo("service supervisor start")
+
+    # READ LOCAL CONFIG FILE, ALTER IT FOR THIS MACHINE RESOURCES, AND PUSH TO REMOTE
+    conf_file = File("./examples/config/etl_supervisor.conf")
+    content = conf_file.read_bytes()
+    find = between(content, "numprocs=", "\n")
+    content = content.replace("numprocs=" + find + "\n", "numprocs=" + str(cpu_count) + "\n")
+    File("./temp/etl_supervisor.conf.alt").write_bytes(content)
+    conn.sudo("rm -f /etc/supervisor/conf.d/etl_supervisor.conf")
+    conn.put("./temp/etl_supervisor.conf.alt", '/etc/supervisor/conf.d/etl_supervisor.conf', use_sudo=True)
+    conn.run("mkdir -p /home/ubuntu/ActiveData-ETL/results/logs")
+
+    # POKE supervisor TO NOTICE THE CHANGE
+    conn.sudo("supervisorctl reread")
+    conn.sudo("supervisorctl update")
+
+
+def _add_private_file(conn):
+    conn.run('rm -f /home/ubuntu/private.json')
+    conn.put('~/private_active_data_etl.json', '/home/ubuntu/private.json')
+    with conn.cd("/home/ubuntu"):
+        conn.run("chmod o-r private.json")
 
 
 def main():

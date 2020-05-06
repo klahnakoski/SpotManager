04ecf9b4bf810002cf3201979e1d75e98af76553
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-05-06 05:48:21 -0400

    update script

--- examples/scripts/run_etl.sh
+++ examples/scripts/run_etl.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=.:vendor
 cd ~/SpotManager-ETL
-python spot/spot_manager.py --settings=./examples/config/etl_settings.json 2>~/SpotManager/etl.error.log
+python spot/spot_manager.py --settings=./examples/config/etl_settings.json 2>~/SpotManager-ETL/examples/logs/etl.error.log
 
